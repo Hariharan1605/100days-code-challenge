524abd2859c906a7cc01d8e5bc77654538f1c10d
Hariharan1605 <137361743+Hariharan1605@users.noreply.github.com>
2023-06-22 20:52:36 +0530

    Day 0

---
+++ even or not.py
@@ -0,0 +1,5 @@
+n = int(input("Enter n : "))
+if n%2 == 0 :
+    print("Yes")
+else :
+    print("No")
---
+++ sum of 3 numbers in all combinations.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+num3 = int(input("Enter num3 : "))
+print(num1+num2)
+print(num1+num3)
+print(num2+num3)
