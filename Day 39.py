e1221a26cfd26d7b14a1402a776b2482e4c51be5
Hariharan1605 <137361743+Hariharan1605@users.noreply.github.com>
2023-07-31 15:54:02 +0530

    Day 39

---
+++ Day 39.py
@@ -0,0 +1,31 @@
+# User function Template for python3
+
+class Solution:
+    def select(self, arr, i):
+        pass
+
+    def selectionSort(self, arr, n):
+        for i in range(0, n - 1):
+            for j in range(i + 1, n):
+                if arr[i] > arr[j]:
+                    temp = arr[i]
+                    arr[i] = arr[j]
+                    arr[j] = temp
+        return arr
+
+    # {
+
+
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i], end=" ")
+        print()
+# } Driver Code Ends
